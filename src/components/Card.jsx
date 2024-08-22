421aba987e7d40254105cf87f34d2a5d3db0e2cb
Ilhmrmdhnn <113292768+Ilhmrmdhnn@users.noreply.github.com>
2024-08-22 11:47:27 +0700

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,10 +13,16 @@ import porto12 from '../assets/card12.png';
 import porto13 from '../assets/card13.png';
 import porto14 from '../assets/card14.png';
 import porto15 from '../assets/card15.png';
+import porto16 from '../assets/card16.png';
+import porto17 from '../assets/card17.png';
 
 const Card = () => {
   return (
     <div className="flex flex-col lg:w-9/12 lg:flex-wrap gap-y-4 lg:flex-row w-full mb-20">
+      <div className="text-center my-8">
+        <span className="text-lg font-semibold">Data Science</span>
+      </div>
+
       <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
         <div className="lg:flex-1 relative">
           <a href="https://github.com/Ilhmrmdhnn/Classification-Car" target="_blank" className="block relative">
@@ -37,6 +43,7 @@ const Card = () => {
           </a>
         </div>
       </div>
+
       <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
         <div className="lg:flex-1 relative">
           <a href="https://github.com/Ilhmrmdhnn/Project_Healtcare_Dataset_Stroke" target="_blank" className="block relative">
@@ -57,6 +64,26 @@ const Card = () => {
           </a>
         </div>
       </div>
+
+      <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
+        <div className="lg:flex-1 relative">
+          <a href="https://github.com/Ilhmrmdhnn/Global-Air-Quality-Analytics" target="_blank" className="block relative">
+            <img className="w-full rounded-lg" src={porto15} alt="card" />
+            <div className="text-center mt-2 dark:text-dtitle">Global Air Quality Analytics</div>
+          </a>
+        </div>
+        <div className="lg:flex-1 relative">
+          <a href="https://github.com/Ilhmrmdhnn/Next-Word-Prediction-using-Bidirectional-LSTM-BI-LSTM-" target="_blank" className="block relative">
+            <img className="w-full rounded-lg" src={porto16} alt="card" />
+            <div className="text-center mt-2 dark:text-dtitle">Next Word Prediction using Bidirectional LSTM (BI-LSTM)</div>
+          </a>
+        </div>
+      </div>
+
+      <div className="text-center my-8">
+        <span className="text-lg font-semibold">Data Analyst</span>
+      </div>
+
       <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
         <div className="lg:flex-1 relative">
           <a href="https://github.com/Ilhmrmdhnn/Dashboard-CashFlow" target="_blank" className="block relative">
@@ -111,12 +138,13 @@ const Card = () => {
           </a>
         </div>
         <div className="lg:flex-1 relative">
-          <a href="https://github.com/Ilhmrmdhnn/Global-Air-Quality-Analytics" target="_blank" className="block relative">
-            <img className="w-full rounded-lg" src={porto15} alt="card" />
-            <div className="text-center mt-2 dark:text-dtitle">Global Air Quality Analytics</div>
+          <a href="https://github.com/Ilhmrmdhnn/Pizza-Store-Sales-Dashboard-with-Power-BI" target="_blank" className="block relative">
+            <img className="w-full rounded-lg" src={porto17} alt="card" />
+            <div className="text-center mt-2 dark:text-dtitle">Pizza Store Sales Dashboard with Power BI</div>
           </a>
         </div>
       </div>
+      
       {/* View more link */}
       <div className="text-center">
         <a href="https://github.com/Ilhmrmdhnn" target="_blank" className="text-title dark:text-dtitle text-xl font-medium mb-3 hover:underline">View more &rarr;</a>
--- src/components/fragments/profileFragments.jsx
+++ src/components/fragments/profileFragments.jsx
@@ -15,6 +15,7 @@ export const ProfileInformation = () => {
     <div className="flex justify-center items-center md:items-start md:tracking-widest flex-col text-center mb-10">
       <h1 className="text-title dark:text-dtitle text-2xl font-medium mb-3">Ilham Ramadhan Nur Ahmad</h1>
       <p className="text-subtitle dark:text-dsubtitle tracking-wider text-base md:text-lg text-base mb-3">Business Mathematics - Industrial Mathematics</p>
+      <p className="text-subtitle dark:text-dsubtitle tracking-wider text-base md:text-lg text-base mb-3">Data Enthusiast | Data Analyst | Data Science</p>
       <div className="flex text-subtitle w-full dark:text-dsubtitle justify-center items-center space-x-4">
         <a href="https://github.com/Ilhmrmdhnn" target="_blank" className="flex justify-center items-center">
           <BsGithub className="cursor-pointer text-xl hover:text-title dark:hover:text-dtitle duration-300 ease-in-out" />
